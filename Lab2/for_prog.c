07e190c470c0981e5db3a4acf3cd714a02a75a7c
NitinKarthik01 <nitinsrikarthik@gmail.com>
2026-08-22 20:22:52 +0530

    22/08/26

--- Lab2/for_prog.c
+++ Lab2/for_prog.c
@@ -8,3 +8,39 @@ int main(void)
     }
     return 0;
 }
+
+
+/*
+   0:   b480            push    {r7}
+   2:   b083            sub     sp, #12
+   4:   af00            add     r7, sp, #0
+    int sum=0;
+   6:   2300            movs    r3, #0
+   8:   607b            str     r3, [r7, #4]
+    int i;
+    for(i=1; i<=10; i++)
+   a:   2301            movs    r3, #1
+   c:   603b            str     r3, [r7, #0]
+   e:   e006            b.n     1e <main+0x1e>   -> here the i=0, it is an unconditional jump statement so it jumps to '1e' to check the condition if i<=10
+    {
+        sum=sum+i;
+  10:   687a            ldr     r2, [r7, #4]
+  12:   683b            ldr     r3, [r7, #0]
+  14:   4413            add     r3, r2
+  16:   607b            str     r3, [r7, #4]
+    for(i=1; i<=10; i++)
+  18:   683b            ldr     r3, [r7, #0]
+  1a:   3301            adds    r3, #1
+  1c:   603b            str     r3, [r7, #0]
+  1e:   683b            ldr     r3, [r7, #0]     -> the condition checking starts here
+  20:   2b0a            cmp     r3, #10          -> the condition is checked
+  22:   ddf5            ble.n   10 <main+0x10>   -> branch if less than or equal to then it jumps to line '10' and execution start, this process will continue untill the iteration fails
+    }
+    return 0;
+  24:   2300            movs    r3, #0
+  26:   4618            mov     r0, r3
+  28:   370c            adds    r7, #12
+  2a:   46bd            mov     sp, r7
+  2c:   bc80            pop     {r7}
+  2e:   4770            bx      lr
+*/
--- Lab2/if_prog.c
+++ Lab2/if_prog.c
@@ -8,3 +8,36 @@ int main()
     }
     b = a;
 }
+
+
+/*
+   0:   b480            push    {r7}    -> pushing content of r7 to stack memory as a back up
+   2:   b083            sub     sp, #12    -> we are creating space for the local variables in stack
+   4:   af00            add     r7, sp, #0    -> and referring r7 to the adrees where stack pointer is there to access the created space 
+    int a = 5;
+   6:   2305            movs    r3, #5
+   8:   607b            str     r3, [r7, #4]
+    int b = 10;
+   a:   230a            movs    r3, #10
+   c:   603b            str     r3, [r7, #0]
+    if(a==0)
+   e:   687b            ldr     r3, [r7, #4]
+  10:   2b00            cmp     r3, #0    -> we are comapring the value at [r7,#4] with zero
+  12:   d102            bne.n   1a <main+0x1a>    -> branch if not equals to : jump to 1a
+    {
+        a = a+10;
+  14:   687b            ldr     r3, [r7, #4]
+  16:   330a            adds    r3, #10
+  18:   607b            str     r3, [r7, #4]
+    }
+    b = a;
+  1a:   687b            ldr     r3, [r7, #4]
+  1c:   603b            str     r3, [r7, #0]
+  1e:   2300            movs    r3, #0   -> the main function is of int, so it should return a value, so by default based on the compiler it make the register to zero now 
+  20:   4618            mov     r0, r3   -> and after the retun statment executes it makes the r0 register to zero
+  22:   370c            adds    r7, #12  -> it goes back again to the staring position after clearing the space 
+  24:   46bd            mov     sp, r7   -> sp point to the address of r7 and access the content of r7 it initially stored as backup
+  26:   bc80            pop     {r7}     -> now it pops the ocntents of r7 and places back in r7 register
+  28:   4770            bx      lr       -> it goes back to the link register address( the caller of the function)
+  
+  */
